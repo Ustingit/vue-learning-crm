d60bde31a5e3fb8bf20ea125ee4938bfbcf425bc
ustinovich.yuryi <ustinhtc@gmail.com>
2020-12-21 10:57:24 +0300

    12th - add color for dynamic source for planning page

--- src/store/record.js
+++ src/store/record.js
@@ -10,6 +10,17 @@ export default {
                 commit('setError', e) //call mutation 
                 throw e
             }
+        },
+        async fetchRecords({commit, dispatch}) {
+            try {
+                const uid = await dispatch('getUid')
+                var response = (await firebase.database().ref(`/users/${uid}/records`).once('value')).val() || {}
+
+                return Object.keys(response).map(key => ({...response[key], id: key}))
+            } catch(e) {
+                commit('setError', e)
+                throw e
+            }
         }
     }
 }
--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -3,23 +3,69 @@
 <div>
   <div class="page-title">
     <h3>Планирование</h3>
-    <h4>12 212</h4>
+    <h4>{{info.bill | currencyfilter('RUB')}}</h4>
   </div>
 
-  <section>
-    <div>
+  <Loader v-if="loading" />
+  <p v-else-if="!categories.length" >Категорий пока нет. <router-link to="/categories" >добавить категорию</router-link></p>
+  <section v-else >
+    <div v-for="cat in categories" :key="cat.id" >
       <p>
-        <strong>Девушка:</strong>
-        12 122 из 14 0000
+        <strong>{{cat.title}}:</strong>
+        {{cat.spend | currencyfilter}} из {{cat.limit | currencyfilter}}
       </p>
       <div class="progress" >
         <div
-            class="determinate green"
-            style="width:40%"
+            class="determinate" :class="[cat.progresColor]"
+            :style="{width: cat.progresPercent + '%'}"
         ></div>
       </div>
     </div>
   </section>
 </div>
-
 </template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Planning',
+  data: () => ({
+    loading: true,
+    categories: []
+  }),
+  async mounted() {
+    var records = await this.$store.dispatch('fetchRecords')
+    var categories = await this.$store.dispatch('fetchCategories')
+
+    this.categories = categories.map(cat => {
+      const spend = records
+      .filter(x => x.categoryId === cat.id)  //how much money did we spend for currnt category
+      .filter(x => x.type === 'outcome')
+      .reduce((total, record) => {
+        return total += +record.sum //to avoid string concatenation if we got string
+      }, 0); 
+
+      const percent = 100 * spend / cat.limit;
+      const progresPercent = percent > 100 ? 100 : percent;  //avoi progre more than 100% in case of troubles
+      const progresColor = progresPercent < 60 ?
+        'green' : 
+        progresPercent < 100 
+          ? 'yellow'
+          : 'red';
+
+        return {
+          ...cat,
+          progresPercent,
+          progresColor,
+          spend
+        }
+    })
+
+    this.loading = false
+  },
+  computed: {
+    ...mapGetters(['info']) //we can call this ith name 'info'
+  }
+}
+</script>
