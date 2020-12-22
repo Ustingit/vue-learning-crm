a2e79828420b39efb5ca07f8e2d9f65e3ae381da
ustinovich.yuryi <ustinhtc@gmail.com>
2020-12-22 11:09:05 +0300

    17th- open the page with parameters plus breadcrumbs

--- src/views/DetailRecord.vue
+++ src/views/DetailRecord.vue
@@ -1,27 +1,54 @@
 <template>
-    
 <div>
-  <div>
+  <Loader v-if="loading" />
+  <div v-else-if="record" >
     <div class="breadcrumb-wrap">
       <a href="/history" class="breadcrumb">История</a>
       <a class="breadcrumb">
-        Расход
+        {{ record.type === 'outcome' ? 'Расход' : 'Доход' }}
       </a>
     </div>
     <div class="row">
       <div class="col s12 m6">
-        <div class="card red">
+        <div class="card" :class="record.type === 'outcome' ? 'red' : 'green'" >
           <div class="card-content white-text">
-            <p>Описание:</p>
-            <p>Сумма:</p>
-            <p>Категория:</p>
+            <p>Описание: {{record.description}}</p>
+            <p>Сумма: {{record.sum | currencyfilter('RUB') }}</p>
+            <p>Категория: {{record.categoryName}}</p>
 
-            <small>12.12.12</small>
+            <small>{{record.date}}</small>
           </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 
+  <p v-else class="center" >Запись с айди = {{$route.params.id}} не существует</p>
+</div>
 </template>
+
+<script>
+import Loader from '../components/app/Loader.vue'
+export default {
+  components: { Loader },
+  name: 'DetailedRecord',
+  data: () => ({
+    loading: true,
+    record: null
+  }),
+  async mounted() {
+    const id = this.$route.params.id
+    if (id) {
+      const record = await this.$store.dispatch('fetchRecordById', id)
+      const category = await this.$store.dispatch('fetchCategoryById', record.categoryId)
+      
+      this.record = {
+        ...record,
+        categoryName: category.title
+      }
+
+      this.loading = false
+    }
+  }
+}
+</script>
